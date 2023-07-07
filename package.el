cde56dfdc5b8c665fc9f3e0ff57afe6bd94316bc
Bernd Wachter <bernd-github@wachter.fi>
2023-07-07 14:48:37 +0300

    Export helper: Handle subdirectories

--- package.el
+++ package.el
@@ -2,9 +2,10 @@
   "Export all org files to html. This expects files to have #+EXPORT_FILE_NAME: set correctly."
   (let ((org-files (directory-files-recursively "." (rx ".org"))))
     (mapcar (lambda (file)
-              (with-temp-buffer
-                (insert-file-contents file)
-                (org-html-export-to-html))) org-files)))
+              (let ((default-directory (file-name-directory (expand-file-name file))))
+                (with-temp-buffer
+                  (insert-file-contents (file-name-nondirectory file))
+                  (org-html-export-to-html)))) org-files)))
 
 (defun make-zip()
   "Pack up everything that should be part of the extension in a zip file. Zip
