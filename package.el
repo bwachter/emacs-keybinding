43702738fe41473f793c2ab28fce6676bd251383
Bernd Wachter <bernd-github@wachter.fi>
2023-07-18 13:44:49 +0300

    Make package script executable from shell

--- package.el
+++ package.el
@@ -1,3 +1,7 @@
+#!/usr/bin/emacs --script
+
+(require 'org)
+
 (defun make-html()
   "Export all org files to html. This expects files to have #+EXPORT_FILE_NAME: set correctly."
   (let ((org-files (directory-files-recursively "." (rx ".org"))))
@@ -13,5 +17,7 @@ output will be available in the *zip* buffer."
   (let ((zip-files (directory-files-recursively "." (rx (or ".html" ".js" ".json" ".png" "icons" "LICENSE")))))
     (apply #'call-process "zip" nil "*zip*" nil "-r" "-FS" "../Emacs-keybinding.zip" (append zip-files))))
 
+(message "Building html pages...")
 (make-html)
+(message "Building zip...")
 (make-zip)
